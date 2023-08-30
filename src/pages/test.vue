fb06e20102ebd7ea75559009c523a39ce9d02c90
caobaichuan1122 <caobaichuan1122@gmail.com>
2023-08-30 12:04:33 +1000

    update web version control

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,15 +1,55 @@
 <template>
-  <div>
-    <div class="grid-container">
-      <div class="logo">
-        <!-- Your logo content -->
-        <img src="@/assets/logo.png" alt="" />
+  <div class="box_12 flex-col">
+    <div class="nav-bar_2 flex-row">
+      <img
+          class="image_2"
+          referrerpolicy="no-referrer"
+          src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng049064115903a357059c820bc7483d7e353bcfc21447d7b8fc82f482fb91c2c6"
+      />
+      <span class="text_2">Fleet&nbsp;manage&nbsp;system</span>
+      <div class="icon_1 flex-col"></div>
+    </div>
+    <div class="section_2 flex-col">
+      <span class="text_3">Charge&nbsp;list</span>
+    </div>
+    <div class="box_3 flex-col">
+      <div class="group_1 flex-row">
+        <div class="text-wrapper_1 flex-col">
+          <span class="text_4">1</span>
+        </div>
+        <span class="text_5">123456789（CHARGE&nbsp;ID）</span>
+        <img
+            class="switch_1"
+            referrerpolicy="no-referrer"
+            src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPnge9462864e547c705ed2496170fe61d75164ad56c4e9f46404e9035f123a3dcc0"
+        />
       </div>
-      <div v-for="(page, pageIndex) in paginatedItems" :key="pageIndex" class="page">
-        <div v-for="item in page" :key="item.id" class="grid-item">
-          <!-- Your content for each div item -->
-          {{ item.text }}
-          <button :style="{ height: item.buttonHeight }">{{ item.buttonText }}</button>
+      <div class="group_2 flex-row justify-between">
+        <div class="box_6 flex-col"></div>
+        <span class="text_6">Online</span>
+      </div>
+      <div class="group_3 flex-row">
+        <div class="text-group_7 flex-col">
+          <span class="text_7">220&nbsp;V</span>
+<!--          <span class="text_8">Voltage</span>-->
+        </div>
+        <img
+            class="image_3"
+            referrerpolicy="no-referrer"
+            src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng2e65ab9700a99f1496415eddfcd5cfddc7fda651173c5a60b642447f56170095"
+        />
+        <div class="text-group_8 flex-col">
+          <span class="text_9">75&nbsp;Kw</span>
+<!--          <span class="text_10">Power</span>-->
+        </div>
+        <img
+            class="image_4"
+            referrerpolicy="no-referrer"
+            src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng2e65ab9700a99f1496415eddfcd5cfddc7fda651173c5a60b642447f56170095"
+        />
+        <div class="text-group_9 flex-col">
+          <span class="text_11">30&nbsp;Kwh</span>
+<!--          <span class="text_12">Electicity</span>-->
         </div>
       </div>
     </div>
@@ -43,67 +83,239 @@ export default {
 </script>
 
 <style scoped>
-.grid-container {
-  position: relative;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-start;
+.box_12 {
+  margin-bottom: 33px;
+  padding: 13px 0 1px 0;
 }
 
-.logo {
-  width: 100%;
+.nav-bar_2 {
+  width: 374px;
+  //align-items: center;
+  margin: 14px 0 0 1px;
+  padding: 15px 14px 6px 14px;
+  display: flex;
+  align-items: center;
+}
+
+.flex-row {
+  display: flex;
+  align-items: center;
+}
+
+.image_2 {
+  width: 36px;
+  height: 14px;
+  margin: 5px 0 5px 0;
+}
+
+.text_2 {
+  overflow-wrap: break-word;
+  color: rgba(0, 0, 0, 1);
+  font-size: 18px;
+  //font-family: PingFangSC-Medium;
+  font-weight: 500;
   text-align: center;
-  margin-bottom: 20px;
+  white-space: nowrap;
+  line-height: 24px;
+  margin-left: 46px;
 }
 
-.grid-item {
-  width: 400px;
-  height: 300px;
-  background-color: #9dcbec;
-  margin-left: 40px;
-  margin-top: 30px;
-  border-radius: 20px;
-  overflow: hidden;
-  display: flex;
-  margin-bottom: 100px;
-  box-shadow: 10px 10px 50px rgba(0, 0, 0, 0.3);
+.icon_1 {
+  //border-radius: 50%;
+  background-image: url(https://lanhu-dds-backend.oss-cn-beijing.aliyuncs.com/merge_image/imgs/58e9daebe9364284a9c5d71b4d9d1b8b_mergeImage.png);
+  width: 22px;
+  height: 22px;
+  margin: 1px 0 1px 65px;
 }
 
-.page {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-}
-.grid-item {
-  width: 365px;
-  height: 300px;
-  //background-color: #fffdfd;
-  margin-left: 10px;
-  //margin-top: 30px;
-  border-radius: 20px;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column; /* 垂直堆叠子元素 */
-  justify-content: space-between; /* 垂直均匀分布子元素 */
-  align-items: center; /* 水平居中子元素 */
-  margin-bottom: 100px;
-  box-shadow: 10px 10px 50px rgba(0, 0, 0, 0.3);
+.section_2 {
+  background: url(https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng13c423bd59eb11eebf8f0dd99fad3ebc0162a615d750416d71caa50d987d032f)
+  0px 0px no-repeat;
+  background-size: 375px 182px;
+  margin-top: 5px;
+  position: relative;
+  padding: 127px 267px 30px 15px;
 }
 
-.grid-item button {
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  padding: 5px 10px;
-  background-color: transparent;
-  cursor: pointer;
-  background-color: #f7fafa;
-  transition: background-color 0.3s, color 0.3s;
-  margin-top: auto; /* 将按钮推到容器底部 */
-  margin-bottom: 10px; /* 在按钮底部添加一些距离 */
+.text_3 {
+  overflow-wrap: break-word;
+  color: rgba(0, 0, 0, 1);
+  font-size: 18px;
+  //font-family: PingFangSC-Medium;
+  font-weight: 500;
+  text-align: center;
+  white-space: nowrap;
+  line-height: 24px;
 }
 
-.grid-item button:hover {
-  background-color: #ccc;
-  color: white;
+.box_3 {
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 8px;
+  height: 138px;
+  width: 346px;
+  position: absolute;
+  //left: 15px;
+  //top: 161px;
+  padding: 16px 13px 16px 13px;
+}
+
+.group_1 {
+}
+
+.text-wrapper_1 {
+  background-color: rgba(39, 164, 58, 1);
+  border-radius: 50%;
+  padding: 1px 7px 1px 7px;
+}
+
+.text_4 {
+  overflow-wrap: break-word;
+  color: rgba(255, 255, 255, 1);
+  font-size: 16px;
+  //font-family: DINAlternate-Bold;
+  font-weight: 700;
+  text-align: center;
+  white-space: nowrap;
+  line-height: 20px;
+}
+
+.text_5 {
+  overflow-wrap: break-word;
+  color: rgba(0, 0, 0, 1);
+  font-size: 15px;
+  //font-family: PingFangSC-Medium;
+  font-weight: 500;
+  text-align: left;
+  white-space: nowrap;
+  line-height: 20px;
+  margin: 1px 0 0 12px;
+}
+
+.switch_1 {
+  width: 38px;
+  height: 20px;
+  margin: 1px 0 1px 57px;
+}
+
+.group_2 {
+  width: 48px;
+  margin: 7px 238px 0 34px;
+}
+
+.box_6 {
+  background-color: rgba(39, 164, 58, 1);
+  border-radius: 50%;
+  width: 5px;
+  height: 5px;
+  margin: 5px 0 4px 0;
+}
+
+.text_6 {
+  overflow-wrap: break-word;
+  color: rgba(39, 164, 58, 1);
+  font-size: 12px;
+  //font-family: PingFangSC-Medium;
+  font-weight: 500;
+  text-align: center;
+  white-space: nowrap;
+  line-height: 14px;
+}
+
+.group_3 {
+  margin: 20px 14px 0 8px;
+}
+
+.text-group_7 {
+}
+
+.text_7 {
+  overflow-wrap: break-word;
+  color: rgba(0, 0, 0, 1);
+  font-size: 16px;
+  //font-family: DINAlternate-Bold;
+  font-weight: 700;
+  text-align: center;
+  white-space: nowrap;
+  line-height: 19px;
+  align-self: center;
+}
+
+.text_8 {
+  overflow-wrap: break-word;
+  color: rgba(115, 121, 129, 1);
+  font-size: 13px;
+  //font-family: PingFangSC-Regular;
+  //font-weight: NaN;
+  text-align: center;
+  white-space: nowrap;
+  line-height: 18px;
+  margin-top: 6px;
+}
+
+.image_3 {
+  width: 1px;
+  height: 22px;
+  margin: 11px 0 10px 35px;
+}
+
+.text-group_8 {
+  margin-left: 39px;
+}
+
+.text_9 {
+  overflow-wrap: break-word;
+  color: rgba(0, 0, 0, 1);
+  font-size: 16px;
+  //font-family: DINAlternate-Bold;
+  font-weight: 700;
+  text-align: center;
+  white-space: nowrap;
+  line-height: 19px;
+}
+
+.text_10 {
+  overflow-wrap: break-word;
+  color: rgba(115, 121, 129, 1);
+  font-size: 13px;
+  //font-family: PingFangSC-Regular;
+  //font-weight: NaN;
+  text-align: center;
+  white-space: nowrap;
+  line-height: 18px;
+  margin: 6px 2px 0 1px;
+}
+
+.image_4 {
+  width: 1px;
+  height: 22px;
+  margin: 11px 0 10px 40px;
+}
+
+.text-group_9 {
+  margin-left: 39px;
+}
+
+.text_11 {
+  overflow-wrap: break-word;
+  color: rgba(0, 0, 0, 1);
+  font-size: 16px;
+  //font-family: DINAlternate-Bold;
+  font-weight: 700;
+  text-align: center;
+  white-space: nowrap;
+  line-height: 19px;
+  margin: 0 1px 0 2px;
+}
+
+.text_12 {
+  overflow-wrap: break-word;
+  color: rgba(115, 121, 129, 1);
+  font-size: 13px;
+  //font-family: PingFangSC-Regular;
+  //font-weight: NaN;
+  text-align: center;
+  white-space: nowrap;
+  line-height: 18px;
+  margin-top: 6px;
 }
 </style>
